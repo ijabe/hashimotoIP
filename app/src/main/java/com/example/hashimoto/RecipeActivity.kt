2b7d8420634bcd37057e3655d63bc3b9aae959d7
ijabe <izabella.biesiada@gmail.com>
2022-09-15 23:47:24 +0200

    nowe

--- app/src/main/java/com/example/hashimoto/RecipeActivity.kt
+++ app/src/main/java/com/example/hashimoto/RecipeActivity.kt
@@ -1,11 +1,10 @@
 package com.example.hashimoto
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+
 
 class RecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,17 +17,14 @@ class RecipeActivity : AppCompatActivity() {
         val bCurry =findViewById<CardView>(R.id.cvBreakfastRecipeItem1)
         val bFish = findViewById<CardView>(R.id.cvBreakfastRecipeItem2)
         bThaiSoup.setOnClickListener{
-
             val intent = Intent(this, ThaiActivity::class.java)
             startActivity(intent)
         }
         bCurry.setOnClickListener{
-
             val intent = Intent(this, CurryActivity::class.java)
             startActivity(intent)
         }
         bFish.setOnClickListener{
-
             val intent = Intent(this, FishActivity::class.java)
             startActivity(intent)
         }
